6a24391cb1ed781c153c14229e0fea0710c76708
Андрей <honor10love2@gmail.com>
2022-11-16 20:22:52 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName ;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println("Задача 2");
+        String fullNameBig = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameBig);
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+
+
+
+
+
+    }
+}
